8161bc15dba4eb4833a37e807e31272961a84ac4
Fangzhou Hu <f9hu@ucsd.edu>
2019-11-23 22:13:04 -0800

    some changes

--- code/web-driver/selenium_demo.py
+++ code/web-driver/selenium_demo.py
@@ -2,20 +2,25 @@ import time
 import csv
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-
-
-page_url = "https://siccode.com/"
+from py2neo import Graph, Node, Relationship
+from selenium.webdriver.common.keys import Keys
+g = Graph(password="1234")
+page_url = "https://www.google.com/"
 
 
 driver = webdriver.Chrome('./chromedriver')
 
 company_list = ["Microsoft","Apple","Uber", "Youtube"]
+person_name_list = g.run("MATCH (p:Person) RETURN p.name as name").data()
 
-for company in company_list:
+for person in person_name_list:
     driver.get(page_url)
-    searchBox = driver.find_element_by_id("keyword_m")
-    searchBox.send_keys(company)
-    driver.find_element_by_xpath("/html/body/div[1]/article/section[1]/div/form/div/div[2]/input").click()
-    companyCode = driver.find_element_by_xpath("//*[@id='result-naics']/ul/li/a/div").text
-    print(companyCode)
-    time.sleep(2)
+    searchBox = driver.find_element_by_xpath('//*[@id="tsf"]/div[2]/div[1]/div[1]/div/div[2]/input')
+    searchBox.send_keys(person['name'])
+    searchBox.send_keys(Keys.RETURN)
+    try:
+        dob = driver.find_element_by_xpath('//*[@id="rhs"]/div/div[1]/div[1]/div[1]/div/div[2]/div/div[3]/div/div/span[2]').text
+        print(person['name'] +":" +dob)
+        time.sleep(2)
+    except:
+          print("ERROR")
